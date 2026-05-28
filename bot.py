db22bdf592f47193d679dfdea4b499704b181f0a
ebelova83-blip <e.belova83@icloud.com>
2026-05-28 15:32:36 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,11 @@ OWNER_ID = 8595521730
 async def forward_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
 user = update.effective_user
 text = update.message.text
+await context.bot.send_message(
+    chat_id=OWNER_ID,
+    text=f"📩 Новый анонимный вопрос:\n\n{text}"
+    )
+    await update.message.reply_text("Сообщение отправлено анонимно ✅")
 app = ApplicationBuilder().token(TOKEN).build()
 
 app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, forward_message))
