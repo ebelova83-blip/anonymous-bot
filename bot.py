667db63947414221035afb172b4997c9de959ab6
ebelova83-blip <e.belova83@icloud.com>
2026-05-28 16:13:40 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,21 +1,25 @@
 from telegram import Update
-from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
-TOKEN = "8887585380:AAEdehG1nB0slEWw_X_xZWm-MXI-nJtm_Kg"
+TOKEN = "8887585380:AAEdehG1nB0slEWw_X_xZWm-MXI-nJtm_Kg" мы
 OWNER_ID = 8595521730
 
 async def forward_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.message.text
 
     await context.bot.send_message(
-        chat_id=OWNER_ID
+        chat_id=OWNER_ID,
         text=f"📩 Новый анонимный вопрос:\n\n{text}"
     )
 
-    await update.message.reply_text("Сообщение отправлено анонимно ✅")
+    await update.message.reply_text("Сообщение отправлено анонимно 👌")
 
 app = ApplicationBuilder().token(TOKEN).build()
-app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, forward_message))
+
+app.add_handler(
+    MessageHandler(filters.TEXT & ~filters.COMMAND, forward_message)
+)
 
 print("Бот запущен")
+
 app.run_polling()
