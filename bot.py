aa6a6c5f7de60081df8a5a3fc9f634867ff73924
ebelova83-blip <e.belova83@icloud.com>
2026-05-28 15:59:28 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,14 +1,14 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, ContextTypes, filters
 
-TOKEN = "TOKEN = "8887585380:AAEdehG1nB0slEWw_X_xZWm-MXI-nJtm_Kg"
+TOKEN = "8887585380:AAEdehG1nB0slEWw_X_xZWm-MXI-nJtm_Kg"
 OWNER_ID = 8595521730
 
 async def forward_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     text = update.message.text
 
     await context.bot.send_message(
-        chat_id=OWNER_ID,
+        chat_id=OWNER_ID
         text=f"📩 Новый анонимный вопрос:\n\n{text}"
     )
 
